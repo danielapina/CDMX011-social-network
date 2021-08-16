b2de58de8694e7573b15bbbf385c3ad8aa25617c
DianaAraujoG <dianac251@gmail.com>
2021-08-16 15:11:35 -0500

    primera parte de ruteo

--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
+import template from './template.js';
 // Este es el punto de entrada de tu aplicacion
 const database = firebase.firestore();
 console.log(database);
 
-document.getElementById('register-page').style.display = 'block';
-document.getElementById('home-page').style.display = 'none';
+// Routeo ----------------------------------------------------------------------------
+const rootDiv = document.getElementById('root');
+rootDiv.innerHTML = template[window.location.pathname];
 
+const onNavigate = (pathname) => {
+  window.history.pushState({}, pathname, window.location.origin + pathname);
+  rootDiv.innerHTML = template[pathname];
+};
+console.log(onNavigate);
+// Este ayuda a llamar a la funcion apenas carge la pagina
+window.onload = onNavigate('/home');
+
+window.onpopstate = () => {
+  rootDiv.innerHTML = template[window.location.pathname];
+};
+// Aqui pense en hacerlo con clases pero ya me dio hambre
+const btnRegister = document.getElementById('btn-register');
+
+btnRegister.addEventListener('click', (evt) => { 
+  evt.preventDefault();
+  onNavigate(btnRegister.value);
+});
+
+// REGISTRO -----------------------------------------------------------------
 const registerForm = document.getElementById('register-form');
 
 registerForm.addEventListener('submit', async (e) => {
