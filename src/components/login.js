d79229dc89eff4aeaeadc539db8f7ff930a0c55b
fanita90 <parroquini_90@hotmail.com>
2021-08-25 17:29:48 -0500

    se agregó la funcionalidad de mostrar la contraseña

--- src/components/login.js
+++ src/components/login.js
@@ -7,13 +7,8 @@ export const login = () => {
      <form  id="login-form" class ="form-inicial">
        <h2 class="titles" id="title-form">¡Hola Green Friend!</h2>
        <img id="heart-movil" src="img/heart.png" alt="heart" />
-<<<<<<< HEAD
-       <label for="email">Correo electrónico</label>
-       <input type="email" id="login-email" />
-=======
        <label for="email">Correo electronico</label>
        <input type="email" id="login-email" placeholder='alguien@ejemplo.com'/>
->>>>>>> main
        <label for="password">Contraseña</label>
        <input type="password" id="login-password" placeholder="******"/>
        <br>
