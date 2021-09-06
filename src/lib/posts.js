a3297f32b3d706f44be3260297a929efe732a210
fanita90 <parroquini_90@hotmail.com>
2021-09-06 18:58:54 -0500

    cambios en lib post js

--- src/lib/posts.js
+++ src/lib/posts.js
@@ -3,3 +3,15 @@ import firebase from './secret.js';
 export const db = firebase.firestore();
 
 export const getAllPost = () => db.collection('post');
+
+// -------------------------------posts------------------
+
+export const newPost = (user, topic, idea) => db.collection('post').doc().set({
+  datePublic: new Date(),
+  user,
+  topic,
+  idea,
+  likes: [],
+});
+export const getThePost = (id) => db.collection('post').doc(id).get();
+export const deletePost = (id) => db.collection('post').doc(id).delete();
